a0dba850e85ceae7602db7ee8150904a8d251600
SkayLarsen <SkayLarsen@gmail.com>
2017-01-07 15:28:17 +0300

    задача #8

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,32 +1,44 @@
 import vk
+from getpass import getpass
 
-
-APP_ID = -1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
+APP_ID = -1
 
 
 def get_user_login():
-    pass
+    return input('Login: ')
 
 
 def get_user_password():
-    pass
+    return getpass()
 
 
-def get_online_friends(login, password):
-    session = vk.AuthSession(
+def vk_authorize(login, password):
+    return vk.AuthSession(
         app_id=APP_ID,
         user_login=login,
         user_password=password,
+        scope="friends"
     )
+
+
+def get_online_friends(session):
     api = vk.API(session)
-    # например, api.friends.get()
+    friends_online_ids = api.friends.getOnline()
+    return api.users.get(user_ids=friends_online_ids)
 
 
 def output_friends_to_console(friends_online):
-    pass
+    for friend in friends_online:
+        print(friend['first_name'], friend['last_name'])
+
 
 if __name__ == '__main__':
-    login = get_user_login()
-    password = get_user_password()
-    friends_online = get_online_friends(login, password)
-    output_friends_to_console(friends_online)
+    try:
+        login = get_user_login()
+        password = get_user_password()
+        vk_session = vk_authorize(login, password)
+        friends_online = get_online_friends(vk_session)
+        print("\nДрузья онлайн:\n")
+        output_friends_to_console(friends_online)
+    except:
+        print("Приложение завершилось с ошибкой")
